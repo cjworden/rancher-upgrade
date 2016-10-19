7483291beabcab811218a897a722ba4ddd9a65ae
Cody Worden <cjworden@greatergood.com>
2016-10-19 11:02:38 -0700

    Adding errors as return values.

---
+++ errors.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type actionAvailableError struct {
+	Action  string
+	Service string
+}
+
+type upgradeError struct {
+	Action string
+	Service string
+	Err error
+}
+
+func (e *actionAvailableError) Error() string {
+	return fmt.Sprintf("Action %s is not available on service %s", e.Action, e.Service)
+}
+
+func (e *upgradeError) Error() string {
+	return fmt.Sprintf("Error trying to upgrade %s during the %s action.\n\n%e", e.Service, e.Action, e.Err)
+}
